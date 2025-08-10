f3a33e98293b3af3261c4fa071c3f9b7585d102b
Michael Gunkoff <painter@3lines.club>
2025-08-10 22:27:06 +0200

    update tests

--- model_test.go
+++ model_test.go
@@ -12,25 +12,26 @@ func TestEncode(t *testing.T) {
 	}{
 		{"_", nil},
 		{"a", nil},
-		{"bbb", nil},
+		{"bbbe", nil},
 		{"1", nil},
 		{"-", invalidCharacter('-')},
-		{"aaaaaaaaaaaa", ErrInvalidLength},
-		{"aaaaaaaaaa", nil},
+		{"aa1aaaaaaaaa", ErrInvalidLength},
+		{"", nil},
+		{"aaaaaaaaba", nil},
 		{"aaaaaaaaa_", nil},
 		{"aaaaa aaaa", nil},
 		{"AAAA aaa_", nil},
-		{"", ErrEmptyString},
 	}
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := Encode(tt.input)
 			if !errors.Is(err, tt.err) {
 				t.Errorf("Encode() error = %v, wantErr %v", err, tt.err)
+				return
 			}
-
-			if err == nil && got.String() != tt.input {
-				t.Errorf("Encode() got = %v, want %v", got, tt.input)
+			gotString := got.String()
+			if err == nil && gotString != tt.input {
+				t.Errorf("Encode() got = %s, want %v", gotString, tt.input)
 			}
 		})
 	}
